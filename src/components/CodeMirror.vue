c61cda0ad8b6aec682311766eb6ac323235276d2
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-01-07 23:42:58 +0000

    Quick attempt at implementing CodeMirror

---
+++ src/components/CodeMirror.vue
@@ -0,0 +1,35 @@
+<template>
+  <textarea id="code">
+    Hello World!
+  </textarea>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+
+export default Vue.extend({
+  name: 'DataInput',
+  mounted() {
+    const codeMirror = CodeMirror.fromTextArea(
+      document.getElementById('code') as HTMLTextAreaElement,
+      {
+        value: 'Hello World! 2.0',
+        lineNumbers: true,
+        mode: 'javascript',
+      },
+    );
+    codeMirror.on('change', (instance) => {
+      // eslint-disable-next-line no-console
+      console.log(instance);
+      this.$emit('change', 'Hello?');
+    });
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
--- src/components/DataInput.vue
+++ src/components/DataInput.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-textarea
-    name="data"
-    auto-grow
-    solo
-    :value.sync="value"
-    @input="update"
-  />
+  <div>
+    <code-mirror />
+    <v-textarea
+      name="data"
+      auto-grow
+      solo
+      :value.sync="value"
+      @input="update"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import CodeMirror from '@/components/CodeMirror.vue';
 
 export default Vue.extend({
   name: 'DataInput',
+  components: {
+    CodeMirror,
+  },
   props: {
     value: {
       type: String,

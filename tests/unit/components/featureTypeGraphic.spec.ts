732e89bbc5e38eebb56abd5939e2048c68f85b6b
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-02-20 23:09:08 +0000

    Added unit test for FeatureTypeGraphic

---
+++ tests/unit/components/featureTypeGraphic.spec.ts
@@ -0,0 +1,31 @@
+import { mount, Wrapper } from '@vue/test-utils';
+import FeatureTypeGraphic from '@/components/FeatureTypeGraphic.vue';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let wrapper: Wrapper<any>;
+
+describe('CopyOutputButton', () => {
+  beforeAll(() => {
+    wrapper = mount(FeatureTypeGraphic, {
+      propsData: {
+        featureType: 'points',
+        selected: true,
+      },
+    });
+  });
+
+  test('mounts with no errors', async () => {
+    await wrapper.vm.$nextTick();
+  });
+
+  test('featureType can be switched', async () => {
+    wrapper.setProps({
+      featureType: 'linestring',
+    });
+    await wrapper.vm.$nextTick();
+  });
+
+  afterAll(() => {
+    wrapper.destroy();
+  });
+});

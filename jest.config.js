00d5d85a6456c1957b242f78bce9f3b5c8a0431b
Leif Gehrmann <leif.gehrmann@gmail.com>
2020-10-26 08:37:06 +0000

    Added jest-transform-stub to handle codemirror CSS imports

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.vue'],
+  transform: {
+    '.+\\.(css)$': 'jest-transform-stub',
+  },
+  moduleNameMapper: {
+    '.+\\.(css)$': 'jest-transform-stub',
+  },
 };
